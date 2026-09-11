77fd4e3aeb8e423d8feb2744d6f944986a970993
najibbouchama-byte <najib.bouchama@gmail.com>
2026-09-11 23:20:53 +0200

    Update LiveTvScreen.kt

--- app/src/main/java/com/iptvplayer/app/ui/livetv/LiveTvScreen.kt
+++ app/src/main/java/com/iptvplayer/app/ui/livetv/LiveTvScreen.kt
@@ -3,6 +3,7 @@ package com.iptvplayer.app.ui.livetv
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Check
