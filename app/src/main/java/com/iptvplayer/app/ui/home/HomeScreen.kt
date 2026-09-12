8d6d7bec7d8e2814e7fe9c190d423037038dbb19
najibbouchama-byte <najib.bouchama@gmail.com>
2026-09-12 13:43:30 +0200

    Update HomeScreen.kt

--- app/src/main/java/com/iptvplayer/app/ui/home/HomeScreen.kt
+++ app/src/main/java/com/iptvplayer/app/ui/home/HomeScreen.kt
@@ -2,6 +2,7 @@ package com.iptvplayer.app.ui.home
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
@@ -29,7 +30,6 @@ import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
-import androidx.compose.material3.LinearProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.OutlinedTextField
